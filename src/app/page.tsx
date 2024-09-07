6733f1906eb34bbbf686ece0d984036fd9cbf573
levi <guncow4336@gmail.com>
2024-09-07 17:30:34 +0900

    [test] Project Setting last

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex flex-col gap-10 bg-amber-50 w-full justify-center items-center h-[100vh]">
         <Image src='/nnzzImage.jpeg' width={300} height={300} alt='nnzz'/>
       <div className="text-xl font-normal text-black text-center">
-          nnzz은 오픈 준비중 ! 박사님들이 열심히 연구중이에요 조금만 기다려주세요 ! 😀🫶
+          nnzz(냠냠쩝쩝)은 오픈 준비중 ! 박사님들이 열심히 연구중이에요 조금만 기다려주세요 ! 😀🫶
       </div>
     </div>
   );
