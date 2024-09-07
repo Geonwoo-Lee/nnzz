0827b924f5734b7c7f0b99b6939e92a6bc192f49
levi <guncow4336@gmail.com>
2024-09-07 16:59:02 +0900

    [deploy]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="text-2xl font-bold flex justify-center items-center h-[100vh]">
-      Coming Soon !
+    <div className="text-2xl font-bold flex flex-col gap-2 bg-amber-50 w-full justify-center items-center h-[100vh]">
         <Image src='/nnzzImage.jpeg' width={300} height={300} alt='nnzz'/>
+      Coming Soon !
     </div>
   );
 }
