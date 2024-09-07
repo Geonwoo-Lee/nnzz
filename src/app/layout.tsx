3fa10fe55e815722a92045b02e2f0495c91c93ed
levi <guncow4336@gmail.com>
2024-09-07 15:31:29 +0900

    Project build error clear

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
-
 export const metadata: Metadata = {
   title: "nnzz",
   description: "배고픈 여러분! 🤗 냠냠쩝쩝은 여러분의 점심 고민을 해결해 줄 쩝쩝 박사들의 사이드 프로젝트에요 !",
@@ -25,7 +14,6 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body
-        // className={`${geistSans.variable} ${geistMono.variable}`}
       >
         {children}
       </body>
