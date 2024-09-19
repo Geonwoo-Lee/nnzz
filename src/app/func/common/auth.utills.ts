0c5c083db4b85ed0bb83ed3fe457a3845631da47
levi <guncow4336@gmail.com>
2024-09-19 17:31:12 +0900

    [push] api fetch 코드, 로그인 테스트 완료

---
+++ src/app/func/common/auth.utills.ts
@@ -0,0 +1,56 @@
+import {LoginRes, UserInfo} from "@/src/app/types/common/user";
+
+export default class AuthUtils {
+    public static isLoggedIn() {
+        return this.getToken() != null && this.getUserInfo() != null;
+    }
+
+    private static TOKEN_KEY = "토큰";
+
+    public static setToken(token: string) {
+        localStorage.setItem(this.TOKEN_KEY, JSON.stringify(token));
+    }
+
+    public static getToken(): string | null {
+        const tokenStr = localStorage.getItem(this.TOKEN_KEY);
+        return tokenStr ? JSON.parse(tokenStr) : null;
+    }
+
+    public static removeToken() {
+        localStorage.removeItem(this.TOKEN_KEY);
+    }
+
+
+
+    public static setUserInfo(UserInfo: UserInfo) {
+        //todo userInfo 설정
+        localStorage.setItem("userInfo", JSON.stringify(UserInfo));
+    }
+
+    public static getUserInfo(): UserInfo | null {
+        //todo 토큰명 설정
+        const storedUserInfo = localStorage.getItem("userInfo");
+        if (!storedUserInfo) {
+            return null;
+        }
+        try {
+            return JSON.parse(storedUserInfo);
+        } catch (e) {
+            return null;
+        }
+    }
+
+    public static removeUserInfo() {
+        localStorage.removeItem("토큰");
+    }
+
+    public static async login(loginRes: LoginRes) {
+        return new Promise((resolve) => {
+            this.setToken(loginRes.token.token);
+            this.setUserInfo({
+                ...loginRes.member,
+            });
+            resolve(true);
+        });
+    }
+}
