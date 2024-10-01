c86e5a5f9838bb07998b6929fb5fe9f36e335bce
geonwoolee <guncow4336@gamil.com>
2024-10-01 23:08:28 +0900

    [deploy] landing 화면 버튼과 컴포넌트 겹치지 않게 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,36 +5,29 @@ import Nnzz from '../../public/svg/logo/Nnzzsvg.svg'
 import dynamic from "next/dynamic";
 import Image from "next/image";
 
-
-
 export default function Home() {
-
     const KakaoButton = dynamic(() => import('@/src/app/component/client/button/KakaoButton'), { ssr: false });
     const HomeCarousel = dynamic(() => import('@/src/app/component/page/home/carousel/HomeCarousel'), {ssr: false});
 
-
-
-  return (
-    <div className="flex flex-col gap-10 bg-white w-full justify-center items-center ">
-      {/*<div className="text-xl font-normal text-black text-center">*/}
-      {/*    nnzz(냠냠쩝쩝)은 오픈 준비중 ! 박사님들이 열심히 연구중이에요 조금만 기다려주세요 ! 😀🫶*/}
-      {/*</div>*/}
-      {/*  <Deck cards={freeFoodData}/>*/}
-      <div className="h-[100vh] w-full flex flex-col items-center justify-center relative">
-             <div className='flex flex-col justify-center items-center gap-8  pt-16 pb-16'>
-                 <Image width={128} height={128} src={'/images/logo/nnzz-home-logo.png'}  alt='nnzz-logo'/>
-                 <div className='flex flex-col  justify-center items-center gap-4 text-common font-bold text-md'>
-                     <Nnzz/>
-                     직장인들 점심 저녁 메뉴 고민을 함께 해요🧚‍
-                 </div>
-             </div>
-          <HomeCarousel/>
-          <div className='absolute bottom-10 w-full pl-4 pr-4'>
-              <KakaoButton/>
-          </div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen  bg-white">
+            <main className="flex-grow flex flex-col items-center justify-center pb-20"> {/* pb-20을 추가하여 KakaoButton을 위한 공간 확보 */}
+                <div className="h-full w-full flex flex-col items-center justify-center">
+                    <div className='flex flex-col justify-center items-center gap-8 pt-16 pb-16'>
+                        <Image width={128} height={128} src={'/images/logo/nnzz-home-logo.png'} alt='nnzz-logo'/>
+                        <div className='flex flex-col justify-center items-center gap-4 text-common font-bold text-md'>
+                            <Nnzz/>
+                            직장인들 점심 저녁 메뉴 고민을 함께 해요🧚‍
+                        </div>
+                    </div>
+                    <HomeCarousel/>
+                </div>
+            </main>
+            <footer className="fixed bottom-10 left-0 right-0 p-4 bg-white max-w-[640px] mx-auto">
+                <KakaoButton/>
+            </footer>
+        </div>
+    );
 }
 
 
