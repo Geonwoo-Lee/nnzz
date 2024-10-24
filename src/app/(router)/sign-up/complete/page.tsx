ed2001454fbc7657fa7ea08e41e48a5b8b77b1ad
levi <guncow4336@gmail.com>
2024-10-24 16:28:39 +0900

    [deploy] complete page 수정

---
+++ src/app/(router)/sign-up/complete/page.tsx
@@ -0,0 +1,9 @@
+import SignUpComplete from "@/src/app/component/client/page/sign-up/complete/SignUpComplete";
+
+const CompletePage = () => {
+    return (
+        <SignUpComplete/>
+    )
+}
+
+export default CompletePage
