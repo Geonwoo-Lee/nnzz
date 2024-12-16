0f653d4039d49ec410a43cf35e76df13de4e1116
Geonwoo-Lee <guncow4336@gmail.com>
2024-12-16 19:17:34 +0900

    [deploy] 데이터 수정

--- src/app/dummy/dummy.ts
+++ src/app/dummy/dummy.ts
@@ -8,175 +8,501 @@ const foodData: FoodItemFromClient[] = [
         imageUrl: `${imageLocation}/Bibim.png`,
         bgType: "Pink"
     },
-    // 2. 탕과국
+    {
+        categoryId: 2,
+        imageUrl: `${imageLocation}/Bibim.png`,
+        bgType: "Pink"
+    },
+    {
+        categoryId: 3,
+        imageUrl: `${imageLocation}/Bibim.png`,
+        bgType: "Pink"
+    },
+    {
+        categoryId: 4,
+        imageUrl: `${imageLocation}/Bibim.png`,
+        bgType: "Pink"
+    },
+    {
+        categoryId: 5,
+        imageUrl: `${imageLocation}/Bibim.png`,
+        bgType: "Pink"
+    },
+    {
+        categoryId: 6,
+        imageUrl: `${imageLocation}/Bibim.png`,
+        bgType: "Pink"
+    },
     {
         categoryId: 7,
         imageUrl: `${imageLocation}/Seolungtang.png`,
         bgType: "Yellow"
     },
-    // 3. 찌개,전골
+    {
+        categoryId: 8,
+        imageUrl: `${imageLocation}/Seolungtang.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 9,
+        imageUrl: `${imageLocation}/Seolungtang.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 10,
+        imageUrl: `${imageLocation}/Seolungtang.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 11,
+        imageUrl: `${imageLocation}/Seolungtang.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 12,
+        imageUrl: `${imageLocation}/Seolungtang.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 13,
+        imageUrl: `${imageLocation}/Seolungtang.png`,
+        bgType: "Yellow"
+    },
     {
         categoryId: 14,
         imageUrl: `${imageLocation}/Zzigae.png`,
         bgType: "Red"
     },
-    // 4. 백숙,삼계탕
     {
-        categoryId: 4,
+        categoryId: 15,
         imageUrl: `${imageLocation}/Samgyetang.png`,
         bgType: "Yellow"
     },
-    // 5. 냉면
     {
         categoryId: 16,
         imageUrl: `${imageLocation}/Nengmyun.png`,
         bgType: "Green"
     },
-    // 6. 국수와 면류
     {
         categoryId: 17,
         imageUrl: `${imageLocation}/Kalgooksu.png`,
         bgType: "Yellow"
     },
-    // 7. 샤브샤브
+    {
+        categoryId: 18,
+        imageUrl: `${imageLocation}/Kalgooksu.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 19,
+        imageUrl: `${imageLocation}/Kalgooksu.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 20,
+        imageUrl: `${imageLocation}/Kalgooksu.png`,
+        bgType: "Yellow"
+    },
     {
         categoryId: 21,
         imageUrl: `${imageLocation}/Shabu.png`,
         bgType: "Pink"
     },
-    // 8. 죽
     {
         categoryId: 22,
         imageUrl: `${imageLocation}/Jook.png`,
         bgType: "Red"
     },
-    // 9. 족발,보쌈
     {
-        categoryId: 9,
+        categoryId: 23,
         imageUrl: `${imageLocation}/Suyook.png`,
-        bgType: "Pink"
+        bgType: "Red"
     },
-    // 10. 전,빈대떡
     {
         categoryId: 24,
         imageUrl: `${imageLocation}/Jeon.png`,
         bgType: "Red"
     },
-    // 11. 중식
     {
         categoryId: 25,
         imageUrl: `${imageLocation}/Jjajang.png`,
         bgType: "Yellow"
     },
-    // 12. 일식
+    {
+        categoryId: 26,
+        imageUrl: `${imageLocation}/Jjajang.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 27,
+        imageUrl: `${imageLocation}/Jjajang.png`,
+        bgType: "Yellow"
+    },
     {
         categoryId: 28,
         imageUrl: `${imageLocation}/Sushi.png`,
         bgType: "Blue"
     },
-    // 13. 초밥
+    {
+        categoryId: 29,
+        imageUrl: `${imageLocation}/Sushi.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 30,
+        imageUrl: `${imageLocation}/Sushi.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 31,
+        imageUrl: `${imageLocation}/Sushi.png`,
+        bgType: "Blue"
+    },
     {
         categoryId: 32,
         imageUrl: `${imageLocation}/Sushi.png`,
         bgType: "Red"
     },
-    // 14. 돈가스
     {
         categoryId: 33,
         imageUrl: `${imageLocation}/Donkats.png`,
         bgType: "Pink"
     },
-    // 15. 카레
     {
         categoryId: 34,
         imageUrl: `${imageLocation}/Curry.png`,
         bgType: "Green"
     },
-    // 16. 아시아음식
     {
         categoryId: 35,
         imageUrl: `${imageLocation}/RiceNoodle.png`,
         bgType: "Yellow"
     },
-    // 17. 분식
+    {
+        categoryId: 36,
+        imageUrl: `${imageLocation}/RiceNoodle.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 37,
+        imageUrl: `${imageLocation}/RiceNoodle.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 38,
+        imageUrl: `${imageLocation}/RiceNoodle.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 39,
+        imageUrl: `${imageLocation}/RiceNoodle.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 40,
+        imageUrl: `${imageLocation}/Tteockbokki.png`,
+        bgType: "Pink"
+    },
     {
         categoryId: 41,
         imageUrl: `${imageLocation}/Tteockbokki.png`,
         bgType: "Pink"
     },
-    // 18. 양식
+    {
+        categoryId: 42,
+        imageUrl: `${imageLocation}/Tteockbokki.png`,
+        bgType: "Pink"
+    },
+    {
+        categoryId: 43,
+        imageUrl: `${imageLocation}/Tteockbokki.png`,
+        bgType: "Pink"
+    },
+    {
+        categoryId: 44,
+        imageUrl: `${imageLocation}/Tteockbokki.png`,
+        bgType: "Pink"
+    },
     {
         categoryId: 45,
         imageUrl: `${imageLocation}/Pasta.png`,
         bgType: "Green"
     },
-    // 19. 멕시코,남미음식
+    {
+        categoryId: 46,
+        imageUrl: `${imageLocation}/Pasta.png`,
+        bgType: "Green"
+    },
+    {
+        categoryId: 47,
+        imageUrl: `${imageLocation}/Pasta.png`,
+        bgType: "Green"
+    },
+    {
+        categoryId: 48,
+        imageUrl: `${imageLocation}/Pasta.png`,
+        bgType: "Green"
+    },
+    {
+        categoryId: 49,
+        imageUrl: `${imageLocation}/Pasta.png`,
+        bgType: "Green"
+    },
+    {
+        categoryId: 50,
+        imageUrl: `${imageLocation}/Pasta.png`,
+        bgType: "Green"
+    },
     {
         categoryId: 51,
         imageUrl: `${imageLocation}/Taco.png`,
         bgType: "Blue"
     },
-    // 20. 퓨전음식
     {
         categoryId: 52,
         imageUrl: `${imageLocation}/Fusion.png`,
         bgType: "Black"
     },
-    // 21. 고기
     {
         categoryId: 53,
         imageUrl: `${imageLocation}/Beef.png`,
         bgType: "Pink"
     },
-    // 22. 닭,오리
     {
-        categoryId: 22,
+        categoryId: 54,
+        imageUrl: `${imageLocation}/Beef.png`,
+        bgType: "Pink"
+    },
+    {
+        categoryId: 55,
+        imageUrl: `${imageLocation}/Beef.png`,
+        bgType: "Pink"
+    },
+    {
+        categoryId: 56,
+        imageUrl: `${imageLocation}/Beef.png`,
+        bgType: "Pink"
+    },
+    {
+        categoryId: 57,
+        imageUrl: `${imageLocation}/Beef.png`,
+        bgType: "Pink"
+    },
+    {
+        categoryId: 58,
         imageUrl: `${imageLocation}/Chicken.png`,
         bgType: "Yellow"
     },
-    // 23. 곱창,막창,양
+    {
+        categoryId: 59,
+        imageUrl: `${imageLocation}/Chicken.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 60,
+        imageUrl: `${imageLocation}/Chicken.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 61,
+        imageUrl: `${imageLocation}/Chicken.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 62,
+        imageUrl: `${imageLocation}/Chicken.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 63,
+        imageUrl: `${imageLocation}/Chicken.png`,
+        bgType: "Yellow"
+    },
     {
         categoryId: 64,
         imageUrl: `${imageLocation}/Gobchang.png`,
         bgType: "Yellow"
     },
-    // 24. 해물
+    {
+        categoryId: 65,
+        imageUrl: `${imageLocation}/Gobchang.png`,
+        bgType: "Yellow"
+    },
+    {
+        categoryId: 66,
+        imageUrl: `${imageLocation}/Gobchang.png`,
+        bgType: "Yellow"
+    },
     {
         categoryId: 67,
         imageUrl: `${imageLocation}/Shrimp.png`,
         bgType: "Blue"
     },
-    // 25. 생선회
+    {
+        categoryId: 68,
+        imageUrl: `${imageLocation}/Shrimp.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 69,
+        imageUrl: `${imageLocation}/Shrimp.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 70,
+        imageUrl: `${imageLocation}/Shrimp.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 71,
+        imageUrl: `${imageLocation}/Shrimp.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 72,
+        imageUrl: `${imageLocation}/Shrimp.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 73,
+        imageUrl: `${imageLocation}/Shrimp.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 74,
+        imageUrl: `${imageLocation}/Shrimp.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 75,
+        imageUrl: `${imageLocation}/Shrimp.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 76,
+        imageUrl: `${imageLocation}/Shrimp.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 77,
+        imageUrl: `${imageLocation}/Shrimp.png`,
+        bgType: "Blue"
+    },
     {
         categoryId: 78,
         imageUrl: `${imageLocation}/Hoi.png`,
         bgType: "Blue"
     },
-    // 26. 피자
     {
         categoryId: 79,
         imageUrl: `${imageLocation}/Pizza.png`,
         bgType: "Green"
     },
-    // 27. 햄버거
     {
         categoryId: 80,
         imageUrl: `${imageLocation}/Hamburger.png`,
         bgType: "Blue"
     },
-    // 29. 술집
+    {
+        categoryId: 81,
+        imageUrl: `${imageLocation}/Hamburger.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 82,
+        imageUrl: `${imageLocation}/Hamburger.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 83,
+        imageUrl: `${imageLocation}/Hamburger.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 84,
+        imageUrl: `${imageLocation}/Hamburger.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 85,
+        imageUrl: `${imageLocation}/Hamburger.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 86,
+        imageUrl: `${imageLocation}/Hamburger.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 87,
+        imageUrl: `${imageLocation}/Hamburger.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 88,
+        imageUrl: `${imageLocation}/Hamburger.png`,
+        bgType: "Blue"
+    },
+    {
+        categoryId: 89,
+        imageUrl: `${imageLocation}/Hamburger.png`,
+        bgType: "Blue"
+    },
     {
         categoryId: 90,
         imageUrl: `${imageLocation}/Beer.png`,
         bgType: "Red"
     },
-    // 30. 뷔페
+    {
+        categoryId: 91,
+        imageUrl: `${imageLocation}/Beer.png`,
+        bgType: "Red"
+    },
+    {
+        categoryId: 92,
+        imageUrl: `${imageLocation}/Beer.png`,
+        bgType: "Red"
+    },
+    {
+        categoryId: 93,
+        imageUrl: `${imageLocation}/Beer.png`,
+        bgType: "Red"
+    },
+    {
+        categoryId: 94,
+        imageUrl: `${imageLocation}/Beer.png`,
+        bgType: "Red"
+    },
+    {
+        categoryId: 95,
+        imageUrl: `${imageLocation}/Beer.png`,
+        bgType: "Red"
+    },
+    {
+        categoryId: 96,
+        imageUrl: `${imageLocation}/Beer.png`,
+        bgType: "Red"
+    },
+    {
+        categoryId: 97,
+        imageUrl: `${imageLocation}/Beer.png`,
+        bgType: "Red"
+    },
     {
         categoryId: 98,
         imageUrl: `${imageLocation}/Buffet.png`,
         bgType: "Red"
     },
-    // 31. 다이어트,샐러드
+    {
+        categoryId: 99,
+        imageUrl: `${imageLocation}/Buffet.png`,
+        bgType: "Red"
+    },
+    {
+        categoryId: 100,
+        imageUrl: `${imageLocation}/Buffet.png`,
+        bgType: "Red"
+    },
     {
         categoryId: 101,
         imageUrl: `${imageLocation}/Salad.png`,
